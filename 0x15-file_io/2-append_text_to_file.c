d1ea227ecd99efa9d9190b4d7877764a2467ecab
EllyTech <elly.dan.ed@gmail.com>
2023-11-02 13:44:32 -0700

    C - File I/O

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,41 @@
+98% of storage used … If you run out, you won't have enough storage to create, edit, and upload files. Get 100 GB of storage for ₦950.00 ₦240.00/month for 3 months.
+1-create_file.c
+#include "main.h"
+
+/**
+ * append_text_to_file - appends text at the end of a file
+ * @filename: filename.
+ * @text_content: added content.
+ *
+ * Return: 1 if the file exists. -1 if the fails does not exist
+ * or if it fails.
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	int nletters;
+	int rwr;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		for (nletters = 0; text_content[nletters]; nletters++)
+			;
+
+		rwr = write(fd, text_content, nletters);
+
+		if (rwr == -1)
+			return (-1);
+	}
+
+	close(fd);
+
+	return (1);
+}
